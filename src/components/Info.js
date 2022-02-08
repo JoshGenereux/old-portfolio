6ff9e3c554c801ea6dba4a86cfdcb7218f52aa17
JoshGenereux <josh.genereux13@gmail.com>
2022-02-08 11:49:50 -0700

    added home

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,15 +1,24 @@
 import React from "react";
+import image from '../pictures/computer.png'
 
 const Info = ()=>{
+  const js = 'Javascript'
 
   return(
     <div className='info-container'>
       <section className='info-sect'>
-        <h1>Hello, Im Josh Genereux</h1>
-        <h2>Full stack Javascript Developer</h2>
+        <h1 className='info-h1'>
+          Hello, Im
+          <p className='info-h1-p'> Josh</p>
+        </h1>
+        <h2 className='info-h2'>
+          Full stack
+          <p className='info-h2-p'>{js}</p>
+          Developer
+        </h2>
       </section>
       <section className='pic-sect'>
-        <img className='office-pic' src='../../public/pictures/office.jpg' alt='computer'/>
+        <img alt='office' src={image}/>
       </section>
     </div>
   )
