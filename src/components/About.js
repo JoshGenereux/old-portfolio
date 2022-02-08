59c6ac4359a51534f053d3724860e502aa3f7dfd
JoshGenereux <josh.genereux13@gmail.com>
2022-02-08 15:53:16 -0700

    test

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = ()=>{
       <p className='about-p'>I am an aspiring full stack javascript developer! I got into coding in about april 2021.
       At the time I was doing roadwork in Las Vegas after moving there from Utah. Currently I live back
       in Utah and really enjoying programming! Cars, Motorcycles and video games are a big passion of mine.
-      Ive had a few motorcycles, but had to sell my last one due to moving, hopefully I can get another one soon.</p>
+      Ive had a few motorcycles, but had to sell my last one due to moving, hopefully I can get another one soon</p>
       <button className='about-home-btn'>Home</button>
     </div>
   )
