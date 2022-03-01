2c0579b55334c99e1978a617e6bfe51e1204c8a1
JoshGenereux <josh.genereux13@gmail.com>
2022-03-01 15:23:36 -0700

    added styling

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,18 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 const About = ()=>{
 
   return (
-    <div className='about-container'>
-      <h1 className='about-header'>About ME</h1>
-      <p className='about-p'>I am an aspiring full stack javascript developer! I got into coding in about april 2021.
-      At the time I was doing roadwork in Las Vegas after moving there from Utah. Currently I live back
-      in Utah and really enjoying programming! Cars, Motorcycles and video games are a big passion of mine.
-      Ive had a few motorcycles, but had to sell my last one due to moving, hopefully I can get another one soon.</p>
-      <ScrollIntoView selector='.header'>
-        <button className='about-home-btn'><FontAwesomeIcon icon={faHouse} /></button>
-      </ScrollIntoView>
-    </div>
+    <section className='about-sect'>
+      <div className='about-container'>
+        <h1 className='about-header'>About ME</h1>
+        <p className='about-p'>I am an aspiring full stack javascript developer! I got into coding in about april 2021.
+          At the time I was doing roadwork in Las Vegas after moving there from Utah. Currently I live back
+          in Utah and really enjoying programming! Cars, Motorcycles and video games are a big passion of mine.
+          Ive had a few motorcycles, but had to sell my last one due to moving, hopefully I can get another one soon.</p>
+        <ScrollIntoView selector='.header'>
+          <button className='about-home-btn'><FontAwesomeIcon icon={faHouse} /></button>
+        </ScrollIntoView>
+      </div>
+    </section>
   )
 }
 
